479bdf6e18a5993d758cfe9762f28d846c77a542
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-10-26 15:00:50 -0400

    Add menu

--- bglug.py
+++ bglug.py
@@ -3,7 +3,15 @@ from tkinter import Button, Label, Text, messagebox, Menu
 main = tkinter.Tk()
 messagebox.showinfo("Welcome!", "to the official BGLUG program")
 menubar = Menu(main)
-menubar.add_command(label="Quit!", command=main.quit)
+def hello():
+	print("salut")
+# create a pulldown menu, and add it to the menu bar
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=hello)
+filemenu.add_command(label="Save", command=hello)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=main.quit)
+menubar.add_cascade(label="File", menu=filemenu)
 # display the menu
 main.config(menu=menubar)
 main.mainloop()
